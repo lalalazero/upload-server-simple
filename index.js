6097be56345b8412c507516579fb2e32b90e5b3f
lalalazero <zzzero520@hotmail.com>
2020-03-04 21:44:17 +0800

    一次上传多个文件

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ const upload = multer({ dest: 'dest/' })
 
 const app = express()
 
+// 单个文件上传
 app.options('/asubmit', cors())
 app.post('/asubmit', cors(), upload.single('file'), function (req, res, next) {
     res.json({ id: req.file.filename })
 })
+
+// 多个文件上传
+app.options('msubmit', cors())
+app.post('/msubmit', cors(), upload.array('files', 10), function (req, res, next) {
+    console.log(req.files)
+    let arr = req.files.map(file => file.filename)
+    console.log(arr)
+    res.send(arr)
+})
+
+// 图片预览
 app.get('/preview/:id', cors(), function (req, res, next) {
     res.sendFile(`dest/${req.params.id}`, {
         root: __dirname,
@@ -21,7 +33,8 @@ app.get('/preview/:id', cors(), function (req, res, next) {
         }
     })
 })
-
-app.listen(3000, function () {
+var port = process.env.PORT || 3000
+console.log(`port=${port}`)
+app.listen(port, function () {
     console.log('Example app listening on port 3000!')
 })
